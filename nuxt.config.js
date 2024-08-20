e3e4114f583b2bce10fadd27401816c9afc2b837
Fernanda <sanjaya.dev20@gmail.com>
2024-08-20 23:52:11 +0700

    convert html template to nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -35,6 +35,14 @@ export default {
     '@nuxtjs/tailwindcss',
     '@nuxtjs/dotenv',
   ],
+
+  // postcss: {
+  //   plugins: {
+  //     tailwindcss: {},
+  //     autoprefixer: {},
+  //   },
+  // },
+
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
